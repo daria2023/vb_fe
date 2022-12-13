500ab4613d04a9befb1a097dcb4e5187f4cce577
daria2023 <regin2023@gmail.com>
2022-12-13 10:45:55 +0800

    to server

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://zz-vb.onrender.com/api/',
+    baseURL: 'https://zz-vb.onrender.com/api/'
     // baseURL: 'http://localhost:5000/api'
 });
 
