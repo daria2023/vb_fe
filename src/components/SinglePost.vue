bc3147f72cd2c36e3d7d3dbcd35a264918bc8ad8
daria2023 <regin2023@gmail.com>
2023-02-11 19:20:05 +0800

    pre txt fold

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -230,4 +230,12 @@ p.copy {
     font-size: 0.8rem;
     color: var(--color-blue);
 }
+
+.single-post-content {
+    white-space: pre-wrap;       /* css-3 */
+ white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+ white-space: -pre-wrap;      /* Opera 4-6 */
+ white-space: -o-pre-wrap;    /* Opera 7 */
+ word-wrap: break-word;    
+}
 </style>
